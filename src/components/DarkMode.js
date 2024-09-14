82f45c307ef5a98a4a765ace7e8be7d475f11254
a-edrees <a.edrees@solutionplus.net>
2024-09-14 15:11:45 +0300

    init project

---
+++ src/components/DarkMode.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "../styles/DarkMode.css";
+import "../styles/icons-font.css";
+
+const DarkMode = () => {
+  const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+
+  const setDarkMode = () => {
+    document.querySelector("body").setAttribute("data-theme", "dark-theme");
+  };
+
+  const setLightMode = () => {
+    document.querySelector("body").setAttribute("data-theme", "light-theme");
+  };
+  if (darkThemeMq.matches) {
+    setDarkMode();
+  } else {
+    setLightMode();
+  }
+
+  const toggleTheme = (e) => {
+    if (!darkThemeMq.matches) {
+      if (e.target.checked) setDarkMode();
+      else setLightMode();
+    } else {
+      if (e.target.checked) setLightMode();
+      else setDarkMode();
+    }
+  };
+
+  return (
+    <div className="dark_mode">
+      <input
+        className="dark_mode_input"
+        type="checkbox"
+        id="darkmode_toggle"
+        value=""
+        onChange={toggleTheme}
+      />
+      <label className="dark_mode_label" htmlFor="darkmode_toggle"></label>
+      <div className="icon-icon_half-moon"></div>
+    </div>
+  );
+};
+
+export default DarkMode;
